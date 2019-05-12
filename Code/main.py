2ca0659dbed6301a866f1544b791760e11eff3bd
ankitk28 <43935924+ankitk28@users.noreply.github.com>
2019-05-12 14:14:30 -0500

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -61,6 +61,48 @@ def main():
     rf_train_scores, rf_test_scores = automate.test_for_all_rf(data)
     svm_train_scores, svm_test_scores = automate.test_for_all_svm(data)
     lr_train_scores, lr_test_scores = automate.test_for_all_lr(data)
+    
+    data_dict = dict()
+    dataset_wrk = ['breast', 'prostate'] 
+    for d in dataset_wrk:
+        data_dict[d] = Dataset2("../Data", d, kfold=10) 
+    m_list = ['knn','lr','svm','knn','rf','nb']
+    train_scores = dict()
+    test_scores = dict()
+
+    for d in data_dict:
+        train_scores[d] = dict()
+        test_scores[d] = dict()
+
+    for d in data_dict:
+        print(d,end=":\n")
+        data = data_dict[d]
+        for m in ['knn', 'nb', 'rf', 'svm', 'lr']:
+            print("\t"+str(m))
+            train_scores[d][m], test_scores[d][m]  = automod(data, mode=m)
+    import pickle
+
+    f = open("dump_trainscores_kbest.pkl","wb")
+    pickle.dump(train_scores,f)
+
+    f = open("dump_testscores_kbest.pkl","wb")
+    pickle.dump(test_scores,f)
+
+    f.close()
+    plt_titledict = {"knn":"KNN", "nb":"Naive Bayes", "rf":"Random Forest", "svm":"SVM", "lr":"Logistic Regression"}
+    for d in data_dict:
+        print(d,end=":\n")
+        data = data_dict[d]
+
+        for m in ['nb', 'rf', 'svm', 'lr', 'knn']:
+            plt.figure()
+            plt.plot(list(test_scores[d][m].keys()), list(test_scores[d][m].values()), label = "Test")
+            plt.title("Accuracy of " +str(plt_titledict[m]))
+            plt.xlabel("Number of genes")
+            plt.ylabel("Accuracy")
+            plt.legend()
+            plt.savefig(str("Plots/")+str(d)+"_"+str(m)+".jpg"); 
+            plt.show();
 
 
 if __name__ == "__main__":
